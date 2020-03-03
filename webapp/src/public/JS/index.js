f6e9247be79bda16754b31dafdb70a44e4a453d3
SalmanShaik2668 <salmans@thoughtworks.com>
2020-03-04 00:22:12 +0530

    Add gender change css changes

--- webapp/src/public/JS/index.js
+++ webapp/src/public/JS/index.js
@@ -11,7 +11,7 @@ function createNameInput() {
     let nameInput = document.createElement("input");
     nameInput.type = "text";
     nameInput.placeholder = "Enter Name";
-    nameInput.className = "nameInput"
+    nameInput.className = "nameInput masculineInput";
     return nameInput;
 }
 
@@ -27,47 +27,33 @@ function createGenderSwitch() {
     return switchElem;
 }
 
+const createGenderSpan = () => {
+    let span = document.createElement("span");
+    span.className = "masculine";
+    span.innerText = "♂";
+    return span;
+};
+
 const createNameSection = () => {
     let nameDiv = document.createElement("div");
     nameDiv.className = "nameSection";
     let nameInput = createNameInput();
     let switchElem = createGenderSwitch();
+    let genderSpan = createGenderSpan();
     nameDiv.appendChild(nameInput);
     nameDiv.appendChild(switchElem);
+    nameDiv.appendChild(genderSpan);
     return nameDiv;
 };
 
-function appendInitialNameSections(namesDiv) {
-    let firstNameSection = createNameSection();
-    let secondNameSection = createNameSection();
-    namesDiv.appendChild(firstNameSection);
-    namesDiv.appendChild(secondNameSection);
+function appendInitialNameSections() {
+    addNameSection();
+    addNameSection();
 }
-
-function createButton(innerText) {
-    let button = document.createElement("button");
-    button.type = "button";
-    button.innerText = innerText;
-    return button;
-}
-
-const createButtonsDiv = () => {
-    let buttonsDiv = document.createElement("div");
-    let addButton = createButton("+");
-    let removeButton = createButton("-");
-    buttonsDiv.appendChild(addButton);
-    buttonsDiv.appendChild(removeButton);
-    return buttonsDiv;
-};
-
-const appendButtons = namesDiv => {
-    namesDiv.appendChild(createButtonsDiv());
-};
 
 const showNames = () => {
     let namesDiv = document.querySelector(".names");
-    appendInitialNameSections(namesDiv);
-    appendButtons(namesDiv);
+    appendInitialNameSections();
     showElement(namesDiv)
 };
 
@@ -75,7 +61,60 @@ const showForm = () => {
     showNames();
 };
 
+const addNameSection = () => {
+    let namesDiv = document.querySelector(".names");
+    let nameSection = createNameSection();
+    let numberOfChilds = namesDiv.childElementCount;
+    namesDiv.insertBefore(nameSection, namesDiv.children[numberOfChilds - 2]);
+};
+
+const removeNameSection = () => {
+    let namesDiv = document.querySelector(".names");
+    let numberOfChilds = namesDiv.childElementCount;
+    if (numberOfChilds > 4) {
+        namesDiv.removeChild(namesDiv.children[numberOfChilds - 3]);
+    }
+};
+
+const getRespectiveNameInput = checkbox => {
+    let grandParent = checkbox.parentElement.parentElement;
+    return grandParent.querySelector("input");
+};
+
+const changeColor = (input, span, checked) => {
+    if (checked) {
+        span.innerText = "♀";
+        span.className = "feminine";
+        input.className = input.className.replace("masculineInput", "feminineInput");
+        return;
+    }
+    span.innerText = "♂";
+    span.className = "masculine";
+    input.className = input.className.replace("feminineInput", "masculineInput");
+};
+
+const changeInputColor = ({target}) => {
+    let nameInput = getRespectiveNameInput(target);
+    let genderSpan = nameInput.parentElement.lastChild;
+    changeColor(nameInput, genderSpan, target.checked);
+};
+
+const addEventListenerToSwitches = switches => {
+    let checkboxes = Array.from(switches).map(s => s.querySelector("input"));
+    checkboxes.forEach(c => c.onchange = changeInputColor);
+};
+
+const addEventListeners = () => {
+    let addButton = document.querySelector(".add");
+    let removeButton = document.querySelector(".remove");
+    let genderSwitches = document.querySelectorAll(".switch");
+    addButton.onclick = addNameSection;
+    removeButton.onclick = removeNameSection;
+    addEventListenerToSwitches(genderSwitches);
+};
+
 const onload = () => {
     showForm();
+    addEventListeners();
 };
 window.onload = onload;
