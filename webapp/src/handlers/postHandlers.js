2473a7b282e9139d029e3c2683bb39ba84ae8399
SalmanShaik2668 <salmans@thoughtworks.com>
2020-06-30 17:41:01 +0530

    refactoring

--- webapp/src/handlers/postHandlers.js
+++ webapp/src/handlers/postHandlers.js
@@ -1,23 +1,23 @@
-const { v4: uuid } = require("uuid");
+const {v4: uuid} = require("uuid");
 
 const setParticipants = (req, res) => {
-  const uniqueID = uuid();
-  res.cookie("gameId", uniqueID);
-  let participantsList = req.body;
-  const gameList = req.app.games;
-  gameList.setParticipants(uniqueID, participantsList);
-  res.status(201).send("Created");
+    const uniqueID = uuid();
+    res.cookie("gameId", uniqueID);
+    let participantsList = req.body;
+    const gameList = req.app.games;
+    gameList.setParticipants(uniqueID, participantsList);
+    res.status(201).send("Created");
 };
 
 const setMode = (req, res) => {
-  let mode = req.body.mode;
-  const gameList = req.app.games;
-  const { gameId } = req.cookies;
-  gameList.setMode(gameId, mode);
-  res.status(201).send("Created");
+    let mode = req.body.mode;
+    const gameList = req.app.games;
+    const {gameId} = req.cookies;
+    gameList.setMode(gameId, mode);
+    res.status(201).send("Created");
 };
 
 module.exports = {
-  setParticipants,
-  setMode,
+    setParticipants,
+    setMode,
 };
