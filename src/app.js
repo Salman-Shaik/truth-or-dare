f1656e7ab97f376e50a23283b5fd878796af0a32
SalmanShaik2668 <salmans@thoughtworks.com>
2020-07-24 11:36:09 +0530

    enable leverage browser caching

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "webapp/public")));
+app.use(express.static(path.join(__dirname, "webapp/public"),{ maxAge: 86400000 }));
 app.use(redirectToMobileUIFOrMobile);
 app.use(redirectToHomepageIfNoParticipants);
 app.use(redirectToBoardIfGameIsActive);
